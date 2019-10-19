369bec3f29fecb9053f0bc0d00799f5d4a167812
Uberacy Tottoli <bera@boldtech.com.br>
2019-10-19 17:29:27 -0300

    Revert "Teste"
    This reverts commit d8d3f0698c1ddffd8211723aa2ba79512ab7ed8b.

--- WebApplication2/Models/Teste.cs
+++ WebApplication2/Models/Teste.cs
@@ -5,9 +5,5 @@
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
-        public int A { get; set; }
-        public int B { get; set; }
-        public int C { get; set; }
-        public int D { get; set; }
     }
 }
